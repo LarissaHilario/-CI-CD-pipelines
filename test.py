eda68f0c53d1b3fd92f563b766ef93531dc2e68f
Crescens-Henry <crescencio.ps.code@gmail.com>
2024-06-06 16:21:30 -0600

    Update pytest.py with welcome message test

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import requests
+import pytest
 
 
-def test_welcome():
+def test_root_route():
     response = requests.get("http://127.0.0.1:5000")
     assert response.status_code == 200
-    assert "Cambio nuevo de Crescencio otro mas de prueba" in response.text
